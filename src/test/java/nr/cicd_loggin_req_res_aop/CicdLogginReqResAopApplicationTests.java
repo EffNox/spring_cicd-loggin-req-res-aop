3bba86c06049fdd7e7c26adf687cdd45d09ef79a
EffNox <effnox@gmail.com>
2024-11-27 20:49:08 -0500

    feat: adding test

--- src/test/java/nr/cicd_loggin_req_res_aop/CicdLogginReqResAopApplicationTests.java
+++ src/test/java/nr/cicd_loggin_req_res_aop/CicdLogginReqResAopApplicationTests.java
@@ -1,5 +1,6 @@
 package nr.cicd_loggin_req_res_aop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CicdLogginReqResAopApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() {}
+
+	@Test
+	void test2() {
+		assertEquals(1, 1);
 	}
 
 }
