7a22409583331b22e69fb5ecafc3b32878cd34a7
EffNox <effnox@gmail.com>
2024-11-27 20:44:18 -0500

    [FIRST-COMMIT]

---
+++ src/main/java/nr/cicd_loggin_req_res_aop/CicdLogginReqResAopApplication.java
@@ -0,0 +1,13 @@
+package nr.cicd_loggin_req_res_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLogginReqResAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdLogginReqResAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nr/cicd_loggin_req_res_aop/CicdLogginReqResAopApplicationTests.java
@@ -0,0 +1,13 @@
+package nr.cicd_loggin_req_res_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLogginReqResAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
